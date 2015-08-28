c771e0974864086661b3eb4fc9958758c19dce50
Wilfrido Vidana <woqer88@gmail.com>
2015-08-28 02:46:13 -0500

    mantaining user session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
+require 'pry'
+
 class UsersController < ApplicationController
   def spotify_user
-    @spotify_user = params[:spotify_user] || RSpotify::User.new(request.env['omniauth.auth'])
+    user_hash = request.env['omniauth.auth']
+    if user_hash.nil?
+      @spotify_user = RSpotify::User.find(session[:current_user_id])
+    else
+      @spotify_user = RSpotify::User.new(user_hash)
+      session[:current_user_id] = @spotify_user.id
+    end
+    # binding.pry
+    @spotify_user
   end
 
   def spotify
