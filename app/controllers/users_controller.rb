03ac977bd1566193745754ed14a53ee799bca4b4
Wilfrido Vidana <woqer88@gmail.com>
2015-11-20 22:27:23 -0600

    user controller nicer code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
-  def spotify_user
-    user_hash = request.env['omniauth.auth']
-    if user_hash.nil?
-      @spotify_user = RSpotify::User.find(session[:current_user_id])
-    else
-      @spotify_user = RSpotify::User.new(user_hash)
-      session[:current_user_id] = @spotify_user.id
-    end
-    @spotify_user
-  end
-
   def spotify
-    spotify_user
-
+    session[:current_user_id] = spotify_user.id
   end
 
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+  def spotify_user
+    @spotify_user ||= ( session[:current_user_id] &&
+      RSpotify::User.find(session[:current_user_id]) ) ||
+      RSpotify::User.new(auth_hash)
+  end
 end
