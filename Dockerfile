27fab82f74e742b6e00d79164e458ad9c301b4fb
Wilfrido Vidana <woqer88@gmail.com>
2016-02-05 18:05:01 -0600

    DB migrated to postgre, deployment on Docker  - Simple Dockerfile for Rails  - docker-compose for Postgre and Rails containers

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ruby:2.2.0
+
+RUN apt-get update -qq && apt-get install -y build-essential \
+    libpq-dev nodejs qt5-default libqt5webkit5-dev \
+    gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-x
+
+RUN mkdir /PartyPlaylist
+
+WORKDIR /PartyPlaylist
+
+COPY Gemfile Gemfile.lock ./
+
+RUN gem install bundler && bundle install
+
+ADD . ./
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150829183255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "playlist_entries", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -20,8 +23,8 @@ ActiveRecord::Schema.define(version: 20150829183255) do
     t.integer  "track_id"
   end
 
-  add_index "playlist_entries", ["playlist_id"], name: "index_playlist_entries_on_playlist_id"
-  add_index "playlist_entries", ["track_id"], name: "index_playlist_entries_on_track_id"
+  add_index "playlist_entries", ["playlist_id"], name: "index_playlist_entries_on_playlist_id", using: :btree
+  add_index "playlist_entries", ["track_id"], name: "index_playlist_entries_on_track_id", using: :btree
 
   create_table "playlists", force: :cascade do |t|
     t.string   "spoti_id"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20150829183255) do
     t.integer  "user_id"
   end
 
-  add_index "playlists", ["user_id"], name: "index_playlists_on_user_id"
+  add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
 
   create_table "tracks", force: :cascade do |t|
     t.string   "spoti_id"
@@ -60,7 +63,7 @@ ActiveRecord::Schema.define(version: 20150829183255) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
