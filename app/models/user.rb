40e544f59bfa868f4a75d6ff03fe5b54f33c7a5f
Wilfrido Vidana <woqer88@gmail.com>
2016-02-19 00:44:39 -0600

    cleaning up comments and spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauth_providers => [:spotify]
 
