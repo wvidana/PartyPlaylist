e6a6cb9cd9699992d8e3345e3f1a3765f72644a4
Wilfrido Vidana <woqer88@gmail.com>
2016-02-23 02:44:26 -0600

    Spotify user now is fetched in a better way   1) token is saved into DB on first login   2) next time creedentials are needed, this are fetched from the DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    user.update(spoti_data: session[:spotify_data])
   end
 end
--- app/interactors/spotify_user.rb
+++ app/interactors/spotify_user.rb
@@ -2,9 +2,9 @@ class SpotifyUser
   include Interactor
 
   def call
-    current_user_id = context.session[:current_user_id]
-    user = User.find current_user_id
-    if spoti_user = RSpotify::User.find(user.spoti_id)
+    rspotify_hash = context.rspotify_hash ||
+        User.find_by(spoti_id: context.spoti_user_id).spoti_data
+    if spoti_user = RSpotify::User.new(rspotify_hash)
       context.user = spoti_user
     else
       context.fail!(message: "Spotify User failed! with current_user_id=(#{current_user_id})")
