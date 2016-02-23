48b224ccd0dcf8d180e83d889a849e6244740639
Wilfrido Vidana <woqer88@gmail.com>
2016-02-23 00:56:41 -0600

    fixed update of spoti_data in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    user.spoti_data = session[:spotify_data]
+    user.update(spoti_data: session[:spotify_data])
   end
 end
